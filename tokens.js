2bb67fdadf8ca032c74d7c503151e6f69eebfb11
RichardMCGirt <richard.mcgirt@vanirinstalledsales.com>
2025-03-13 09:26:58 -0400

    Create tokens.js

---
+++ tokens.js
@@ -0,0 +1,7 @@
+{
+  "access_token": "ya29.a0AeXRPp7ty-1aUOjmVbCWP_U29Z6q0WSSKYcbPwG9eqvWJOCa35Kr9q7_3VANZa1x6v78wixL_bkOib4W8LKp1a_pY0OdZxKQN3mIX__ph1BcU_bjvrk6mOaVxFjlkmOIGgMotQlXsQXFVZ4ApIsxs46pI-tfEmk5Xlt0btA-aCgYKAWQSARESFQHGX2MiQ8AqQT3jn3E2wumNTXcbtw0175",
+  "refresh_token": "1//05Obr7y5FWzafCgYIARAAGAUSNwF-L9IrhAPVFE20_qQ4C9S78df4Bqnon3fCrc3YQLTltahKOMMi2Q-1DfLcKZy30uB4S-swRTw",
+  "scope": "https://www.googleapis.com/auth/calendar",
+  "token_type": "Bearer",
+  "expiry_date": 1741875847856
+}
