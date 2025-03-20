06845473f82ed68d81e99646885820fdb80184cc
Richard McGirt <rmcgirt55@gmail.com>
2025-03-20 10:27:40 -0400

    Updates

---
+++ precache-manifest.425ebe260d2ccdfa09ef6fbddb93fe4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73656bb882fc7e417e11d49370570cd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd25d37129ba7e64b1fc",
+    "url": "/static/css/main.8d49095d.chunk.css"
+  },
+  {
+    "revision": "7ff07a24746573b8eb7b",
+    "url": "/static/js/2.b7b37fe2.chunk.js"
+  },
+  {
+    "revision": "e26de4f0e0d98f1626b38316e0457409",
+    "url": "/static/js/2.b7b37fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b1c70c9a150a433962",
+    "url": "/static/js/3.def51621.chunk.js"
+  },
+  {
+    "revision": "c381233c6994fb463cd3",
+    "url": "/static/js/4.a3500ddb.chunk.js"
+  },
+  {
+    "revision": "cd25d37129ba7e64b1fc",
+    "url": "/static/js/main.59167298.chunk.js"
+  },
+  {
+    "revision": "a82ab3511ff34308800b",
+    "url": "/static/js/runtime-main.49693560.js"
+  }
+]);
